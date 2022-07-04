b5ebd1b65cd70f89f565801cf0df3aaf701e151a
Your Name <you@example.com>
2022-07-04 16:23:04 +0300

    ikinci-gonderme

--- article/views.py
+++ article/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request,"index.html")
+    context = {
+        "number1":20
+            }
+    return render(request,"index.html",context)
+
+def about(request):
+    return render(request,"about.html")
+
