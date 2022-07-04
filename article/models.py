0b0a7313b01a3613abbe621e8629c5ce21e9f1e0
Your Name <you@example.com>
2022-07-04 03:43:33 +0300

    ikincigonderme

--- article/models.py
+++ article/models.py
@@ -6,8 +6,9 @@ from sympy import content
 
 
 class Article(models.Model):
-    author = models.ForeignKey("auth.User",on_delete = models.CASCADE ) #model.cascade amacı yazar silinirse makaleyi de sil demek
-    title = models.CharField(max_length=50)
-    content = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-    
+    author = models.ForeignKey("auth.User",on_delete = models.CASCADE,verbose_name="yazar" ) #model.cascade amacı yazar silinirse makaleyi de sil demek
+    title = models.CharField(max_length=50,verbose_name="başlık")
+    content = models.TextField(verbose_name="içerik")
+    created_date = models.DateTimeField(auto_now_add=True,verbose_name="olusturma_tarih")
+    def __str__ (self):
+       return self.title
