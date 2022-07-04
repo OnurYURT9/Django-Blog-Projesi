3646b6e58ed7da0538d5839d8f8e9193b3b4dfcb
Your Name <you@example.com>
2022-07-05 02:30:00 +0300

    ucuncu-gonderme

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "article",
+    "user",
+    'crispy_forms',
+    
 ]
 
 MIDDLEWARE = [
@@ -121,6 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
